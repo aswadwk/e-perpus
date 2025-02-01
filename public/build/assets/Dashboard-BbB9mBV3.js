c4a8e5fda08a6780c988304305c0cbf8b5f9e0db
aswadwk <hajaraswadkom@gmail.com>
2025-02-01 20:57:56 +0700

    update build

--- public/build/assets/Dashboard-BbB9mBV3.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-C3B2Chdh.js";import{B as s}from"./button-D1Fqjazq.js";function o(){return r.jsxs("div",{className:"w-full min-h-screen bg-red-600",children:["Dashboard",r.jsx(s,{children:"Click me"})]})}export{o as default};
